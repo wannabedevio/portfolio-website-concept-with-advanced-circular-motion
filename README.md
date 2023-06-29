9aa89f8987efcdffd3fe0df8bb7f54b9cea49b73
MirzaHodzic <mirzahodzic88@gmail.com>
2023-06-29 10:52:17 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# portfolio-website-concept-with-advanced-circular-motion
+# Portfolio Website Concept with Advanced Circular Motion
+
 An animated portfolio website concept with the circular motion made with HTML5, CSS3, Javascript and Greensock.
+
+![Portfolio Website Concept with Advanced Circular Motion](/assets/img/portfolio-website-concept-with-advanced-circular-motion.png)
+
+[Article on WANNABEDEV](https://wannabedev.io/tutorials/portfolio-website-concept-with-advanced-circular-motion/)
+
+[Demo](https://wannabedev.io/_posts/demo/portfolio-website-concept-with-advanced-circular-motion/)
+
+## Credits
+- [GSAP](https://greensock.com) by Greensock
+- [imagesLoaded](https://imagesloaded.desandro.com/) by David DeSandro
+- Fonts [BioRhyme](https://fonts.google.com/specimen/BioRhyme) & [Space Mono](https://fonts.google.com/specimen/Space+Mono?selection.family=Space+Mono) by Google Fonts
+- Simple Loader by [loading.io](https://loading.io/css/)
+- Color Palette by [coolors.co](https://coolors.co/)
+
+## License
+This resource can be used freely if integrated or build upon in personal or commercial projects such as websites, web apps and web templates intended for sale. It is not allowed to take the resource "as-is" and sell it, redistribute, re-publish it, or sell "pluginized" versions of it. Free plugins built using this resource should have a visible mention and link to the original work. Always consider the licenses of all included libraries, scripts and images used.
+
+## Misc
+
+Follow WANNABEDEV: [GitHub](https://github.com/wannabedevio), [Twitter](https://twitter.com/wannabedev_io), [Instagram](https://www.instagram.com/wannabedev.io/), [TikTok](https://www.tiktok.com/@wannabedev.io), 
+
+[© WANNABEDEV 2023](https://wannabedev.io)
---
+++ assets/js/demo.js
@@ -0,0 +1,79 @@
+/**
+ * demo.js
+ *
+ * Licensed under the MIT license.
+ * https://opensource.org/license/mit/
+ * 
+ * Copyright 2023, WANNABEDEV
+ * https://wannabedev.io
+ */
+
+ function initializeSlideshow() {
+  // Init variables
+  const slideshow = document.querySelector('.slideshow');
+  const loader = document.querySelector('.loader');
+  const navItems = document.querySelectorAll('.nav-item');
+  const background = document.querySelectorAll('.background');
+  const holder = document.querySelector('.holder');
+  const content = document.querySelectorAll('.content');
+  let activeID = null;
+  let activeImage = null;
+  let activeContent = null;
+  let rotation = null;
+  const type = "_short";
+
+  imagesLoaded(slideshow, { background: true }, function() {
+    // Hide loader
+    loader.classList.add('is-loaded');
+
+    // Demo
+    gsap.config({
+      force3D: false
+    });
+
+    // Set tween values
+    function setTweenValues(item) {
+      rotation = Number(item.dataset.rotation);
+      activeID = item.dataset.id;
+      activeImage = item.dataset.bcg;
+      activeContent = slideshow.querySelector(`[data-id='content-${activeID}']`);
+    }
+
+    // Do tween
+    function doTween() {
+      content.forEach(function(elem) {
+        gsap.set(elem, {
+          backgroundImage: ''
+        });
+        gsap.set(elem.querySelector('.inner'), {
+          autoAlpha: 0
+        });
+      });
+
+      const timeline = gsap.timeline();
+      
+      timeline
+        .staggerTo(document.querySelectorAll('.inner'), 0, { autoAlpha: 0, ease: 'power4.inOut' }, 0)
+        .to(activeContent, { duration: 0.75, autoAlpha: 0, ease: 'power4.inOut' }, '-=0.75')
+        .to(activeContent, { duration: 0.75, backgroundImage: `url(${activeImage})` }, '-=0.75')
+        .staggerTo(background, 1.5, { rotation: `${rotation}${type}`, transformOrigin: "100% 100%", ease: 'back.out(1.6)' }, 0.075)
+        .to(activeContent, { duration: 0.75, autoAlpha: 1, rotation: 0, ease: 'power4.inOut' }, '-=1.5')
+        .to(activeContent.querySelector('.inner'), { duration: 0.75, autoAlpha: 1, ease: 'power4.inOut' }, '-=2.5');
+        
+      timeline.play();
+    }
+
+    // Click/hover on items
+    navItems.forEach(function(item) {
+      item.addEventListener('click', function() {
+        doTween();
+      });
+
+      item.addEventListener('mouseenter', function() {
+        setTweenValues(item);
+      });
+    });
+  });
+}
+
+initializeSlideshow();
